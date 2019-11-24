67c40fa4486b00594eed6001d29a4622c64ae655
Patrick Villanueva <pkrvillanueva@gmail.com>
2019-11-25 01:13:41 +0800

    Add user icon default

--- components/Seaman/SeamanList.js
+++ components/Seaman/SeamanList.js
@@ -24,7 +24,7 @@ const columns = [
     dataIndex: 'name',
     render: ( t, r ) => (
       <>
-        <Avatar className={ styles.tableAvatar } src={ r.avatar } shape="square" />
+        <Avatar className={ styles.tableAvatar } src={ r.avatar } shape="square" icon="user" />
         <a href={ `/seaman/${ r.id }` }>
           { r.first_name || r.last_name ? `${ r.first_name } ${ r.last_name }` : '<No Name>' }
         </a>
