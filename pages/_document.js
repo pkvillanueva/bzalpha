91771f7950cface2fd757f53851a46e3fb67410f
Patrick Villanueva <pkrvillanueva@gmail.com>
2019-11-26 16:23:10 +0800

    Simplify transition removal

--- pages/_document.js
+++ pages/_document.js
@@ -9,11 +9,24 @@ class MyDocument extends Document {
   render () {
     return (
       <Html className="no-trans">
-        <Head />
+        <Head>
+          <style
+            id="holderStyle"
+            dangerouslySetInnerHTML={ {
+              __html: `
+                /* https://github.com/ant-design/ant-design/issues/16037#issuecomment-483140458 */
+                /* Not only antd, but also any other style if you want to use ssr. */
+                *, *::before, *::after {
+                  transition: none!important;
+                }
+              `
+            } }
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
-          <script src="/static/chrome-fix.js" />
+          <script src="/static/scripts.js" />
         </body>
       </Html>
     )
