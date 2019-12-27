5e2f89e437303df63abff567cc690292296b8b79
Patrick Villanueva <patrick@artbees.net>
2019-12-27 19:21:08 +0800

    Refactor DataCollection component

--- components/DataCollection/index.js
+++ components/DataCollection/index.js
@@ -1,104 +1,164 @@
 /**
  * External dependencies.
  */
-import React, { useState } from 'react';
-import { Button, Modal, Form, Popconfirm, Card, Table, Divider } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Form, Button, Modal, Table, Divider } from 'antd';
+import { filter, map, isEmpty } from 'lodash';
 
 /**
  * Internal dependencies.
  */
-import { map, filter, find, isEmpty } from 'lodash';
 import styles from './styles.less';
 
 const DataCollection = Form.create()( ( props ) => {
-	const { title, data, columns, modalTitle, modalForm, form, onChange, formatRecord, addPosition, ...table } = props;
-	const { getFieldDecorator, getFieldsValue, resetFields } = form;
-	const [ records, setRecords ] = useState( map( data, ( record, id ) => ( { id, key: id, ...record } ) ) );
+	const {	onSave, initialData, form, modalProps = {}, tableProps = {}, buttonProps = {} } = props;
+	const [ data, setData ] = useState( [] );
 	const [ visible, setVisible ] = useState( false );
-	const [ edit, setEdit ] = useState( {} );
+	const [ loading, setLoading ] = useState( false );
+	const [ editIndex, setEditIndex ] = useState( -1 );
+	let { columns = [] } = props;
 
-	const handleOk = () => {
-		let save = [];
+	useEffect( () => {
+		setData( initialData );
+	}, [] );
+
+	const isEditing = () => {
+		return editIndex >= 0;
+	};
+
+	const getEditingValues = () => {
+		return editIndex < 0 ? {} : data[ editIndex ];
+	};
+
+	const deleteRecord = ( index ) => {
+		const records = filter( data, ( record, i ) => i !== index );
+
+		if ( onSave ) {
+			onSave( {
+				values: records,
+				success() {
+					setData( records );
+				},
+			} );
+		}
+	};
+
+	const handleAdd = () => {
+		setVisible( true );
+	};
+
+	const handleSave = () => {
+		const { getFieldsValue } = form;
 		let values = getFieldsValue();
+		const records = [ ...data ];
 
-		if ( formatRecord ) {
-			values = formatRecord( values );
+		if ( isEmpty( values ) ) {
+			values = {};
 		}
 
-		if ( ! isEmpty( edit ) ) {
-			const id = edit.id;
-			save = [ ...records ];
-			save.splice( id, 1, { id, key: id, ...values } );
+		if ( isEditing() ) {
+			records.splice( editIndex, 1, values );
 		} else {
-			const id = records.length;
-			save = [ ...records, { id, key: id, ...values } ];
+			records.push( values );
 		}
 
-		setRecords( save );
-		onChange( save );
-		setVisible( false );
-		setEdit( {} );
-		resetFields();
+		if ( onSave ) {
+			setLoading( true );
+
+			onSave( {
+				values: records,
+				success() {
+					setVisible( false );
+					setData( records );
+				},
+				error() {
+					setLoading( false );
+				},
+			} );
+		}
 	};
 
 	const handleCancel = () => {
-		resetFields();
 		setVisible( false );
 	};
 
-	const handleNew = () => {
-		setVisible( true );
-		setEdit( {} );
-		resetFields();
-	};
-
-	const handleEdit = ( id ) => {
-		setEdit( find( records, { id } ) );
+	const handleEdit = ( index ) => {
+		setEditIndex( index );
 		setVisible( true );
 	};
 
-	const handleDelete = ( id ) => {
-		const save = filter( records, ( r ) => r.id !== id );
-		setRecords( save );
-		onChange( save );
-	};
-
-	if ( columns[ columns.length - 1 ].key !== 'action' ) {
-		columns.push( {
-			title: 'Action',
-			key: 'action',
-			className: 'action',
-			align: 'center',
-			width: 125,
-			render: ( r ) => (
-				<>
-					<a onClick={ () => handleEdit( r.id ) }>Edit</a>
-					<Divider type="vertical" />
-					<Popconfirm title="Sure to delete?" onConfirm={ () => handleDelete( r.id ) }>
-						<a>Delete</a>
-					</Popconfirm>
-				</>
-			),
+	const handleDelete = ( index ) => {
+		Modal.confirm( {
+			title: 'Are you sure delete this row?',
+			okText: 'Yes',
+			okType: 'danger',
+			cancelText: 'No',
+			onOk: () => deleteRecord( index ),
 		} );
-	}
+	};
+
+	const afterClose = () => {
+		setLoading( false );
+		setEditIndex( -1 );
+	};
+
+	columns = [ ...columns, {
+		title: 'Action',
+		key: 'action',
+		className: 'action',
+		align: 'right',
+		width: 125,
+		render: ( action, record, index ) => (
+			<>
+				<Button type="link" size="small" onClick={ () => handleEdit( index ) }>Edit</Button>
+				<Divider type="vertical" />
+				<Button type="link" size="small" onClick={ () => handleDelete( index ) }>Delete</Button>
+			</>
+		),
+	} ];
 
 	return (
 		<>
 			<Modal
-				visible={ visible }
-				title={ ! isEmpty( edit ) ? `Edit ${ modalTitle }` : `New ${ modalTitle }` }
-				okText="Save"
-				onOk={ handleOk }
-				onCancel={ handleCancel }
 				maskClosable={ false }
+				okText={ 'Save' }
+				{ ...modalProps }
+				title={ isEditing() ? `Edit ${ modalProps.title }` : `New ${ modalProps.title }` }
+				onOk={ handleSave }
+				onCancel={ handleCancel }
+				visible={ visible }
+				okButtonProps={ { loading } }
+				cancelButtonProps={ { disabled: loading } }
+				afterClose={ afterClose }
 			>
-				{ modalForm( getFieldDecorator, edit, form ) }
+				{ modalProps.form( form, getEditingValues() ) }
 			</Modal>
-			<Card title={ title }>
-				{ addPosition !== 'bottom' && <Button className={ styles.newButton } block type="dashed" icon="plus" onClick={ handleNew }>Add New</Button> }
-				<Table className={ styles.table } dataSource={ records } columns={ columns } pagination={ false } scroll={ { x: 'max-content' } } { ...table } />
-				{ addPosition === 'bottom' && <Button className={ styles.newButton } block type="dashed" icon="plus" onClick={ handleNew }>Add New</Button> }
-			</Card>
+			<Button
+				className={ styles.newButton }
+				block={ true }
+				type="dashed"
+				icon="plus"
+				children={ 'Add New' }
+				{ ...buttonProps }
+				onClick={ handleAdd }
+			/>
+			<Table
+				className={ styles.table }
+				dataSource={ map( data, ( record, i ) => ( { key: i, ...record } ) ) }
+				pagination={ false }
+				scroll={ { x: 'max-content' } }
+				{ ...tableProps }
+				columns={ columns }
+			/>
+			<Button
+				className={ styles.newButton }
+				block={ true }
+				type="dashed"
+				icon="plus"
+				children={ 'Add New' }
+				{ ...buttonProps }
+				onClick={ handleAdd }
+			/>
 		</>
 	);
 } );
