745658adcbcece471d9336dfc53a5d3ecf6dd084
Patrick Villanueva <patrick@artbees.net>
2019-12-28 01:06:35 +0800

    Refactor educations and relatives

--- components/Seaman/SeamanEdit.js
+++ components/Seaman/SeamanEdit.js
@@ -11,66 +11,61 @@ import BlockCard from '~/components/BlockCard';
 import PersonalInformation from './Tabs/PersonalInformation';
 import ContactInformation from './Tabs/ContactInformation';
 import EditRelatives from './EditRelatives';
-import Educations from './Tabs/Educations';
+import EditEducations from './EditEducations';
 import EditPassports from './EditPassports';
 import EditVisas from './EditVisas';
 import EditExperiences from './EditExperiences';
 import Licenses from './Tabs/Licenses';
 import BMI from './Tabs/BMI';
 
-const SeamanEdit = () => {
-	return (
-		<Tabs defaultActiveKey="general">
-			<TabPane tab="General" key="general">
-				<BlockCard>
-					<PersonalInformation />
-				</BlockCard>
-				<BlockCard>
-					<ContactInformation />
-				</BlockCard>
-			</TabPane>
-			<TabPane tab="Experiences" key="experiences">
-				<Card title="Experiences">
-					<EditExperiences />
+const SeamanEdit = () => (
+	<Tabs defaultActiveKey="general">
+		<TabPane tab="General" key="general">
+			<BlockCard>
+				<PersonalInformation />
+			</BlockCard>
+			<BlockCard>
+				<ContactInformation />
+			</BlockCard>
+		</TabPane>
+		<TabPane tab="Experiences" key="experiences">
+			<Card title="Experiences">
+				<EditExperiences />
+			</Card>
+		</TabPane>
+		<TabPane tab="Passports" key="passports">
+			<BlockCard>
+				<Card title="Passports">
+					<EditPassports />
 				</Card>
-			</TabPane>
-			<TabPane tab="Passports" key="passports">
-				<BlockCard>
-					<Card title="Passports">
-						<EditPassports />
-					</Card>
-				</BlockCard>
-				<BlockCard>
-					<Card title="VISAS">
-						<EditVisas />
-					</Card>
-				</BlockCard>
-				{ /* <BlockCard>
+			</BlockCard>
+			<BlockCard>
+				<Card title="VISAS">
 					<EditVisas />
-				</BlockCard> */ }
-			</TabPane>
-			<TabPane tab="Licenses" key="documents">
-				<BlockCard>
-					<Licenses />
-				</BlockCard>
-			</TabPane>
-			<TabPane tab="Educations" key="educations">
-				<BlockCard>
-					<Educations />
-				</BlockCard>
-			</TabPane>
-			<TabPane tab="Relatives" key="relatives">
-				<BlockCard>
-					<EditRelatives />
-				</BlockCard>
-			</TabPane>
-			<TabPane tab="BMI" key="bmi">
-				<BlockCard>
-					<BMI />
-				</BlockCard>
-			</TabPane>
-		</Tabs>
-	);
-};
+				</Card>
+			</BlockCard>
+		</TabPane>
+		<TabPane tab="Licenses" key="documents">
+			<BlockCard>
+				<Licenses />
+			</BlockCard>
+		</TabPane>
+		<TabPane tab="Educations" key="educations">
+			<Card title="Educations">
+				<EditEducations />
+			</Card>
+		</TabPane>
+		<TabPane tab="Relatives" key="relatives">
+			<Card title="Relatives">
+				<EditRelatives />
+			</Card>
+		</TabPane>
+		<TabPane tab="BMI" key="bmi">
+			<BlockCard>
+				<BMI />
+			</BlockCard>
+		</TabPane>
+	</Tabs>
+);
 
 export default SeamanEdit;
