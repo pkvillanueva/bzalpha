770c213004b3015d751968fc4124ef835dbe7e86
Patrick Villanueva <pkrvillanueva@gmail.com>
2020-01-13 23:46:20 +0800

    Additional currencies

--- utils/currencies.js
+++ utils/currencies.js
@@ -1,7 +1,7 @@
 import { find } from 'lodash';
 
-export const currencies = [
-	{
+export const currencies = {
+	USD: {
 		symbol: '$',
 		name: 'US Dollar',
 		symbol_native: '$',
@@ -10,7 +10,25 @@ export const currencies = [
 		code: 'USD',
 		name_plural: 'US dollars',
 	},
-];
+	EUR: {
+		symbol: '€',
+		name: 'Euro',
+		symbol_native: '€',
+		decimal_digits: 2,
+		rounding: 0,
+		code: 'EUR',
+		name_plural: 'euros',
+	},
+	PHP: {
+		symbol: '₱',
+		name: 'Philippine Peso',
+		symbol_native: '₱',
+		decimal_digits: 2,
+		rounding: 0,
+		code: 'PHP',
+		name_plural: 'Philippine pesos',
+	},
+};
 
 export const getCurrencySymbol = ( code ) => {
 	const currency = find( currencies, [ 'code', code ] );
