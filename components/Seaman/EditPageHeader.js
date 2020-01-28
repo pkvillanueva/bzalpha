7ac545c83fd0a68c64e0aea1884a9459dd4cf0c2
Patrick Villanueva <pkrvillanueva@gmail.com>
2020-01-28 22:38:55 +0800

    Display correct seaman's status

--- components/Seaman/EditPageHeader.js
+++ components/Seaman/EditPageHeader.js
@@ -10,13 +10,13 @@ import { Statistic, Badge, Tag, Tooltip, Icon } from 'antd';
 import EditAvatar from './EditAvatar';
 import Stats from '../Stats';
 import { SeamanContext } from './store/seaman';
-import { getStatus, getRankName, getTotalSeaTime, getRankTotalSeaTime, getCurrentAge, getContact } from '../../utils/seaman';
+import { getRankName, getTotalSeaTime, getRankTotalSeaTime, getCurrentAge, getContact } from '../../utils/seaman';
+import { getOrderStatus, getOrderStatusColor } from '../../utils/orders';
 import styles from './styles.less';
 
 const EditPageHeader = () => {
 	const { seaman } = useContext( SeamanContext );
 	const { meta } = seaman;
-	const status = getStatus( seaman.job_status );
 
 	return (
 		<div className={ styles.pageHeader }>
@@ -28,16 +28,10 @@ const EditPageHeader = () => {
 					<span>{ `${ meta.first_name } ${ meta.middle_name } ${ meta.last_name }` }</span>
 					{ meta.rank && <Tag color="blue">{ getRankName( meta.rank ) }</Tag> }
 				</h1>
-				{ seaman.job_status === 'onboard' ? (
-					<p>
-						<Badge status={ status.state } /> { status.name }
-						<Tooltip title="Joined Oct 20, 2019 to Feb 15, 2020">
-							<Icon style={ { marginLeft: 8 } } type="info-circle" />
-						</Tooltip>
-					</p>
-				) : (
-					<p><Badge status={ status.state } /> { status.name }</p>
-				) }
+				<p>
+					<Badge status={ getOrderStatusColor( seaman.order ) } />
+					{ getOrderStatus( seaman.order ) }
+				</p>
 			</div>
 			<div className={ styles.extraContent }>
 				<Stats align="right">
